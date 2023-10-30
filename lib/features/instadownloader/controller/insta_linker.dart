fe193a872efc511c9ba5d47aa355ab035bc52b92
Mashood.H <mashoodhussain100@gmail.com>
2023-10-30 17:58:15 +0500

    Update insta_linker.dart

--- lib/features/instadownloader/controller/insta_linker.dart
+++ lib/features/instadownloader/controller/insta_linker.dart
@@ -47,4 +47,42 @@ class InstaLinker extends GetxService {
       return null;
     }
   }
+  Future<File?> downloadFromLinker(
+      {required String url, required String extenstion}) async {
+    try {
+      FlutterInsta flutterInsta = FlutterInsta();
+      String? downloadLink = await flutterInsta.downloadReels(url);
+
+      if (downloadLink != null) {
+        Directory? downloadsDirectoryPath =
+            await DownloadsPath.downloadsDirectory();
+        String fileName = "insta_video_${randomAlpha(15)}$extenstion";
+        final file = File("/${downloadsDirectoryPath!.path}/$fileName");
+        final response =
+            await Dio().get(downloadLink, onReceiveProgress: (received, total) {
+          int percentage = ((received / total) * 100).floor();
+          Get.find<Screencontroller>().updateDownloadProgress(percentage);
+        },
+                options: Options(
+                  maxRedirects: 10,
+                  responseType: ResponseType.bytes,
+                  followRedirects: false,
+                  receiveTimeout: 0,
+                ));
+
+        final raf = file.openSync(mode: FileMode.write);
+
+        raf.writeFromSync(response.data);
+        await raf.close();
+
+        return file;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      // change in production
+      Fluttertoast.showToast(msg: "$e");
+      return null;
+    }
+  }
 }
